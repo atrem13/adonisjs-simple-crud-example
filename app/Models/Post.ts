753b3b8993d4ddd114eca36a11ab88442a955212
atrem13 <mertayoga17.my@gmail.com>
2022-04-19 12:01:45 +0800

    init model

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -5,6 +5,15 @@ export default class Post extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public title:string
+
+  @column()
+  public content:string
+
+  @column()
+  public status: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
